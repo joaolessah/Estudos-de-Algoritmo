5772f307ad88aaa1696f110e985eb720275ffbc5
João Lessa <joaovictorlessa98@gmail.com>
2022-04-24 19:34:09 -0300

    novos exercicios

---
+++ beecrowd/inciante/1094/1094/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+namespace _1094
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, totalCoelhos, totalRatos, totalSapos, total; 
+            double percentRatos, percentCoelhos, percentSapos;
+
+
+
+
+            totalCoelhos = 0;
+            totalRatos = 0;
+            totalSapos = 0;
+
+           
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+
+                string[] vet = Console.ReadLine().Split(' ');
+                int quantia = int.Parse(vet[0]);
+                char animal = char.Parse(vet[1]);
+
+                if (animal == 'C')
+                {
+               
+                    totalCoelhos += quantia;
+                }
+                else if (animal == 'S')
+                {
+                   
+                    totalSapos += quantia;
+                }
+                else 
+                {
+                 
+                    totalRatos += quantia;
+                }
+            }
+
+            
+
+            total = totalCoelhos + totalRatos + totalSapos;
+            percentCoelhos = (double) (totalCoelhos * 100.00) / total;
+            percentRatos = (double) (totalRatos * 100.00)/ total;
+            percentSapos = (double) (totalSapos * 100.00)/ total;
+
+            Console.WriteLine("Total: " + total + " cobaias");
+            Console.WriteLine("Total de coelhos: " + totalCoelhos);
+            Console.WriteLine("Total de ratos: " + totalRatos);
+            Console.WriteLine("Total de sapos: " + totalSapos);
+
+            Console.WriteLine("Percentual de coelhos: " + percentCoelhos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de ratos: " + percentRatos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de sapos: " + percentSapos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+        }
+    }
+}
---
+++ beecrowd/inciante/1132/1132/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _1132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, soma, maior,menor;
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+
+            soma = 0;
+
+           /* if (X < Y)
+            {
+                for (int i = X; i < Y; i++)
+                {
+                    if (i % 13 != 0)
+                    {
+                        soma += i;
+                    }
+                }
+            }
+
+            else if (X > Y)
+            {
+                for (int i = Y; i < X; i++)
+                {
+                    if (i % 13 != 0)
+                    {
+                        soma += i;
+                    }
+                }
+            }
+
+            */
+
+
+            if (X < Y)
+            {
+                menor = X;
+                maior = Y;
+            }
+            else
+            {
+                menor = Y;
+                maior = X;
+            }
+
+            soma = 0;
+
+            for (int i = menor; i < maior; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    soma += i;
+
+                }
+            }
+            
+            /*if (Y % 13 != 0)
+            {
+                soma += Y;
+            }
+
+            else if (X % 13 != 0)
+            {
+
+                soma += X;
+            }
+            else
+            {
+                soma += X + Y;
+            }*/
+            
+            Console.WriteLine(soma);
+        }
+    }
+}
