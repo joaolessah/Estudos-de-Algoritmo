a98c3e773f2db8c1cdae9fe20a5dac488afc6460
João Lessa <joaovictorlessa98@gmail.com>
2022-04-25 23:24:41 -0300

    alteracoes

---
+++ fibonacci-simples/fibonacci-simples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonacci_simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quant_num, contador, anterior, atual, proximo;
+
+            anterior = 0;
+            atual = 1;
+            proximo = 1;
+
+            Console.WriteLine("Digite a quantidade de números desejados: ");
+            quant_num = int.Parse(Console.ReadLine());
+
+            for (contador = 1; contador <= quant_num; contador++)
+            {
+                Console.WriteLine(proximo);
+                proximo = atual + anterior;
+                anterior = atual;
+                atual = proximo;
+            }
+        }
+    }
+}
