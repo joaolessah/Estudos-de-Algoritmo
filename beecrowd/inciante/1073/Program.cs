dc176e8ad7c8cc63e22892daa7d13b14b09be50c
João Lessa <joaovictorlessa98@gmail.com>
2022-08-11 07:53:30 -0300

    desafios 11-08

---
+++ beecrowd/inciante/1073/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class Program {
+
+    static void Main(string[] args) { 
+
+        int n1 = int.Parse(Console.ReadLine());
+        for(int i = 1; i<= n1; i++){
+            if(i % 2 == 0){
+                int quadrado = (int)Math.Pow(i, 2);
+                Console.WriteLine($"{i}^2 = {quadrado}");
+            }
+        }
+    }
+
+}
---
+++ beecrowd/inciante/1078/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class Program {
+
+    static void Main(string[] args) { 
+
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= 10; i++){
+            int mult = i * n;
+            Console.WriteLine($"{i} x {n} = {mult}");
+        }
+
+    }
+
+}
